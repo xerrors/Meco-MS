cc09361784feeb4f9b2ac7fa4acf381f9298c62d
Xerrors <xerrors@163.com>
2021-04-11 18:12:16 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -23,9 +23,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://xerrors.fun:5000/',
+        target: 'https://www.xerrors.fun:5000/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, '/api')
       }
     }
   },
