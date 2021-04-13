8f3efef20b2665656e9b51e6ae01dc8c80acd62a
Xerrors <xerrors@163.com>
2021-04-13 11:10:44 +0800

    update

--- src/views/NewPage.vue
+++ src/views/NewPage.vue
@@ -4,13 +4,13 @@
       <h1>编辑文章</h1>
       <div class="nav-actions nav-btn">
         <a-button
-          @click="myEditor.saveDraft"
-          :loading="draftBtnStatus != '保存草稿'"
+          @click="upload(false)"
+          :loading="draftBtnStatus != '保存'"
           >{{ draftBtnStatus }}</a-button
         >
         <a-dropdown-button
           type="primary"
-          @click="myEditor.upload"
+          @click="upload(true)"
           :loading="btnStatus != '发布'"
         >
           {{ btnStatus }}
@@ -19,12 +19,10 @@
               <a-menu-item key="https://mp.csdn.net/editor/html">
                 转 CSDN
               </a-menu-item>
-              <a-menu-item key="https://juejin.cn">
-                转 掘金
+              <a-menu-item key="https://juejin.cn/editor/drafts/new"> 转 掘金 </a-menu-item>
+              <a-menu-item key="https://www.jianshu.com/writer#/">
+                转 简书
               </a-menu-item>
-              <!-- <a-menu-item key="https://zhuanlan.zhihu.com/write">
-                转 知乎(知乎暂不支持使用markdown)
-              </a-menu-item> -->
             </a-menu>
           </template>
         </a-dropdown-button>
@@ -35,6 +33,7 @@
       v-model="myEditor.text"
       :disabled-menus="[]"
       mode="edit"
+      autofocus
       right-toolbar="preview toc sync-scroll fullscreen"
       @upload-image="handleUploadImage"
       @save="handleSave"
@@ -65,60 +64,63 @@ export default defineComponent({
     let router = useRouter();
 
     const btnStatus = ref("发布");
-    const draftBtnStatus = ref("保存草稿");
+    const draftBtnStatus = ref("保存");
 
     // myEditer 对象
     let myEditor = reactive({
       text: oriText,
-      draft: false,
 
       resetContent: () => {
         myEditor.text = oriText;
       },
+    });
 
-      saveDraft: () => {
-        message.success("功能开发中~");
-        myEditor.draft = true;
+    const toolbar = reactive({
+      clear: {
+        icon: "v-md-icon-clear",
+        title: "重置内容",
+        action(editor: any) {
+          myEditor.resetContent();
+          localStorage.removeItem(String(route.params.path));
+        },
       },
+    });
 
-      publish: () => {
-        myEditor.draft = false;
-      },
+    function upload(compile: boolean) {
+      if (compile) {
+        btnStatus.value = "上传中";
+      } else {
+        draftBtnStatus.value = "上传中";
+      }
 
-      upload: () => {
-        if (!myEditor.draft) {
-          btnStatus.value = "上传中";
-        } else {
-          draftBtnStatus.value = "上传中";
-        }
-
-        // 保存源文件
-        new Promise((resolve, reject): void => {
-          request({
-            url: "/api/admin/articles/md_source",
-            method: "post",
-            data: myEditor.text,
-            headers: {
-              "Content-Type": "text/plain",
-            },
-            params: {
-              path: route.params.path,
-            },
+      // 保存源文件
+      new Promise((resolve, reject): void => {
+        request({
+          url: "/api/admin/articles/md_source",
+          method: "post",
+          data: myEditor.text,
+          headers: {
+            "Content-Type": "text/plain",
+          },
+          params: {
+            path: route.params.path,
+          },
+        })
+          .then((res) => {
+            message.success(res.data.message);
+            if (compile) {
+              btnStatus.value = "编译中";
+            } else {
+              draftBtnStatus.value = "保存";
+            }
+            resolve(res);
           })
-            .then((res) => {
-              message.success(res.data.message);
-              if (!myEditor.draft) {
-                btnStatus.value = "编译中";
-              } else {
-                draftBtnStatus.value = "保存草稿";
-              }
-              resolve(res);
-            })
-            .catch((err) => {
-              reject(err);
-            });
-        });
-        // 编译 vuepress
+          .catch((err) => {
+            reject(err);
+          });
+      });
+      // 编译 vuepress
+      if (compile) {
         new Promise((resolve, reject): void => {
           request({
             url: "/api/admin/deploy-vuepress",
@@ -133,24 +135,10 @@ export default defineComponent({
               reject(err);
             });
         });
-      },
-    });
+      }
+    }
 
-    let toolbar = reactive({
-      clear: {
-        icon: "v-md-icon-clear",
-        title: "重置内容",
-        action(editor: any) {
-          myEditor.resetContent();
-          localStorage.removeItem(String(route.params.path));
-        },
-      },
-    });
-
-    onMounted(() => {
-      // 向服务器获取数据
-      console.log(route.params.path);
-
+    function loadData(): void {
       const key: string = "load_data";
       if (route.params.path == "draft") {
         if (localStorage.getItem("draft")) {
@@ -178,12 +166,21 @@ export default defineComponent({
             });
         });
       }
+    }
+
+    onMounted(() => {
+      // 向服务器获取数据
+      console.log(route.params.path);
+
+      loadData();
     });
 
     return {
       myEditor,
       toolbar,
+      upload,
       btnStatus,
+      draftBtnStatus,
     };
   },
   methods: {
@@ -200,7 +197,11 @@ export default defineComponent({
       //   // height: 'auto',
       // });
     },
+
     handleSave(text: string, html: string) {
+      this.upload(false);
+    },
+    handleChange(text: string, html: string) {
       localStorage.draft = text;
     },
 
@@ -208,7 +209,7 @@ export default defineComponent({
       window.open(link);
     },
 
-    toOtherPlat(e:any): void {
+    toOtherPlat(e: any): void {
       var result = this.myEditor.text;
 
       const coverPatt = /cover:[ ]*.*/;
@@ -217,16 +218,22 @@ export default defineComponent({
       var cover: string = "";
       var permalink: string = "";
 
+      function replace_pun(sta_a: string): string {
+        return sta_a.replace(" ", "")
+                    .replace("'", "")
+                    .replace('"', '')
+      }
+
       var temp = coverPatt.exec(result);
       if (temp) {
-        cover = String(temp).replace(" ", "").slice(6);
+        cover = replace_pun(String(temp)).slice(6);
       } else {
         cover = "";
       }
 
       var temp = permalinkPatt.exec(result);
       if (temp) {
-        permalink = String(temp).replace(" ", "").slice(10);
+        permalink = replace_pun(String(temp)).slice(10);
       } else {
         console.log("There is something WRONG!");
       }
@@ -244,7 +251,7 @@ export default defineComponent({
 
       const { text, isSupported, copy } = useClipboard();
       copy(result);
-      message.success("已经复制到剪贴板，去粘贴吧！", 3);
+      message.success("已经复制到剪贴板，3s后自动跳转~", 3);
       setTimeout(() => {
         this.onOpenNewPage(e.key);
       }, 3000);
