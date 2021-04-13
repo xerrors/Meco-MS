f34b7f57a0e6952947a5f8a5cb1e0e4db72fdabe
Xerrors <xerrors@163.com>
2021-04-14 00:20:24 +0800

    remove

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,8 @@ import App from './App.vue'
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-// import './styles/my-theme-file.less' // 修改部分 antd 自带的样式
 
 import "./styles/index.scss";
-
-// import { VueMarkdownEditor } from '@kangc/v-md-editor';
-// import '@kangc/v-md-editor/lib/style/base-editor.css';
-// import { vuepressTheme } from '@kangc/v-md-editor/lib/theme/vuepress.js';
-// import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
 
 import VMdEditor from '@kangc/v-md-editor';
 import '@kangc/v-md-editor/lib/style/base-editor.css';
